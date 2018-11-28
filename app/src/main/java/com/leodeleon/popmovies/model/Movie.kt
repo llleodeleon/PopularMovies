87d9876de26c6766c1d5b86bf2f839473a7299d8
Leonardo Deleon <leodeleon.dev@outlook.com>
2018-11-28 15:01:06 +0100

    Upgrading project

--- app/src/main/java/com/leodeleon/popmovies/model/Movie.kt
+++ app/src/main/java/com/leodeleon/popmovies/model/Movie.kt
@@ -1,12 +1,13 @@
 package com.leodeleon.popmovies.model
 
 import android.arch.persistence.room.Entity
-import android.os.Parcel
 import android.os.Parcelable
 import com.leodeleon.popmovies.feature.common.AdapterConstants
 import com.leodeleon.popmovies.feature.common.ViewType
+import kotlinx.android.parcel.Parcelize
 
-@Entity(tableName = "movies", primaryKeys = arrayOf("id"))
+@Entity(tableName = "movies", primaryKeys = ["id"])
+@Parcelize
 data class Movie(
     var id : Int = 0,
     var title: String = "",
@@ -17,40 +18,11 @@ data class Movie(
     var vote_average: Double = 0.0,
     var favorite: Int = 0
 ): ViewType, Parcelable {
-
-
   override fun getViewType(): Int {
     return AdapterConstants.MOVIES
   }
 
-  companion object {
-    @JvmField val CREATOR: Parcelable.Creator<Movie> = object : Parcelable.Creator<Movie> {
-      override fun createFromParcel(source: Parcel): Movie = Movie(source.readInt(),
-          source.readString(),
-          source.readString(),
-          source.readString(),
-          source.readString(),
-          source.readString(),
-          source.readDouble(),
-          source.readInt())
-      override fun newArray(size: Int): Array<Movie?> = arrayOfNulls(size)
-    }
-  }
-
   fun isFavorite() = favorite == 1
-
-  override fun describeContents() = 0
-
-  override fun writeToParcel(dest: Parcel, flags: Int) {
-    dest.writeInt(id)
-    dest.writeString(title)
-    dest.writeString(overview)
-    dest.writeString(poster_path)
-    dest.writeString(backdrop_path)
-    dest.writeString(release_date)
-    dest.writeDouble(vote_average)
-    dest.writeInt(favorite)
-  }
 }
 
 data class MovieResponse(
